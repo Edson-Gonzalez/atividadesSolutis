cea37b0ebc323878cdb1035411a8331309ebdbe7
Edson-G <gonzalezedson.junior@gmail.com>
2020-12-10 22:39:12 -0300

    improve array creation

--- javascript1/pratica3.js
+++ javascript1/pratica3.js
@@ -7,24 +7,13 @@ impares os valores do segunda lista.*/
 
 //Comentar se necessário.
 function createArray(){
-    const values = [];
-    for (let i=0; i<10; i++){
-        values[i] = Math.floor(Math.random() * 30);
-    }
-    return values;
+    return new Array(10).fill(null).map(() => Math.floor(Math.random() * 30));
 }
 
 const listaA = createArray();
 const listaB = createArray();
-const listaC = [];
-
-for (let i=0; i<10; i++){
-    if (i%2 == 0){
-        listaC[i] = listaB[i];
-    }else{
-        listaC[i] = listaA[i];
-    }
-}
+const listaC = new Array(10).fill(null).map((current, index) => index%2 == 0 ? 
+    current = listaB[index] : current = listaA[index]);
 
 console.log(listaA);
 console.log(listaB);
