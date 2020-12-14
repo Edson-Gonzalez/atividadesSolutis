a7ad9ab067b6bc3b3aa7d0a6e9c3d3a252ca3c0d
Edson-G <gonzalezedson.junior@gmail.com>
2020-12-14 17:11:59 -0300

    Atividade 14-dez-2020

---
+++ Atividades/14-dez-2020/server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({ extended: true}));
+
+app.listen(4000, function(){
+    console.log('Servidor rodando na porta 4000');
+})
+
+app.get('/', (req, res) => {
+    var cursor = db.collection('usuario').find();
+    res.render('index.ejs');
+})
+ 
+app.get('/show', (req, res) => {
+    db.collection('usuario').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', { data: results })
+ 
+    })
+})
+
+app.route('/edit/:id')
+    .get((req, res) => {
+    var id = req.params.id
+    
+    db.collection('usuario').find(ObjectId(id)).toArray((err, result) => {
+        if (err) return res.send(err)
+        res.render('edit.ejs', { data: result })
+    })
+    })
+    .post((req, res) => {
+    var id = req.params.id
+    var name = req.body.name
+    var surname = req.body.surname
+    
+    db.collection('usuario').updateOne({_id: ObjectId(id)}, {
+        $set: {
+        name: name,
+        surname: surname
+        }
+    }, (err, result) => {
+        if (err) return res.send(err)
+        res.redirect('/show')
+        console.log('Atualizado no Banco de Dados')
+    })
+    })
+
+app.route('/delete/:id')
+    .get((req, res) => {
+      var id = req.params.id
+     
+      db.collection('usuario').deleteOne({_id: ObjectId(id)}, (err, result) => {
+        if (err) return res.send(500, err)
+        console.log('Deletado do Banco de Dados!')
+        res.redirect('/show')
+      })
+    })
+
+app.post('/show', function(req, res){
+    console.log('Teste post...')
+    db.collection('usuario').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+     
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/show')
+      })
+});
+
+app.set('view engine', 'ejs')
+
+const ObjectId = require('mongodb').ObjectID
+ 
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://edson:KsFxcSVXJpPPtJ6F@cluster0.kppho.mongodb.net/cursoReact_Solutis?retryWrites=true&w=majority";
+ 
+MongoClient.connect(uri, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('cursoReact_Solutis') // coloque o nome do seu DB
+ 
+  app.listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+})
